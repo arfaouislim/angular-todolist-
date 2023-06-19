4b61ff523729cf50712e79a3a5b97d4552b67557
Slim ARFAOUI <arfaouislim.tn@gmail.com>
2023-06-19 14:47:17 +0200

    modified the todos list component

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TodoI } from 'src/app/interfaces/todo';
 
 @Component({
   selector: 'app-todos',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class TodosComponent {
 
+  todos :TodoI[] =[
+  { id:"todo-0001",
+    title:"Overview Angular",
+    status:true},
+
+    { id:"todo-0002",
+    title:"Data Binding - Angular",
+    status:false},
+
+    { id:"todo-0003",
+    title:"Observalbles - Angular",
+    status:false},
+  ]
 }
